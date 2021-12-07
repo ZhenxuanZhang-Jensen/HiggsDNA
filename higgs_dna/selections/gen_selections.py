00353bcb37eae775dc9e2319dea2d1ce84324e46
Samuel May <smay@cern.ch>
2021-12-07 23:19:34 +0100

    Update object and gen selections

--- higgs_dna/selections/gen_selections.py
+++ higgs_dna/selections/gen_selections.py
@@ -1,7 +1,5 @@
 import awkward
 import vector
-import numpy
-import numba
 
 vector.register_awkward()
 
@@ -9,9 +7,24 @@ from higgs_dna.utils import awkward_utils
 
 def select_x_to_yz(gen_part, x_pdgId, y_pdgId, z_pdgId):
     """
-    Return all x->yy decays, sorted by x_pt
+    Return all x->yy decays, sorted by x_pt.
+    Values of `None` for any of `x_pdgId`, `y_pdgId`, or `z_pdgId` will result in no requirement on pdgId.
+    For example, 
+        - `x_pdgId = None`, `y_pdgId = 5`, `z_pdgId = 5` will select all X->bb decays
+        - `x_pdgId = None`, `y_pdgId = 11`, `z_pdgId = None` will select all X->eY decays
+
+
     """
 
+    if not isinstance(gen_part, vector.Vector4D):
+        gen_part = awkward.Array(gen_part, with_name = "Momentum4D")
+
+    if x_pdgId is None:
+        x_pdgId = abs(gen_part.pdgId)
+    if y_pdgId is None:
+        y_pdgId = abs(gen_part.pdgId)
+    if z_pdgId is None:
+        z_pdgId = abs(gen_part.pdgId)
 
     gen_yz_from_x = gen_part[((abs(gen_part.pdgId) == y_pdgId) | (abs(gen_part.pdgId) == z_pdgId)) & (abs(gen_part.pdgId[gen_part.genPartIdxMother]) == x_pdgId)]
     gen_yz_from_x = gen_yz_from_x[awkward.argsort(gen_yz_from_x.pt, ascending = False, axis = 1)]
@@ -23,24 +36,12 @@ def select_x_to_yz(gen_part, x_pdgId, y_pdgId, z_pdgId):
     
     # Grab the parent x
     gen_child_pairs["GenParent"] = gen_part[gen_child_pairs.LeadGenChild.genPartIdxMother]
+    gen_child_pairs[("GenParent", "dR")] = gen_child_pairs.LeadGenChild.deltaR(gen_child_pairs.SubleadGenChild) 
 
-    lead_gen_child_p4 = vector.awk({
-        "pt" : gen_child_pairs.LeadGenChild.pt,
-        "eta" : gen_child_pairs.LeadGenChild.eta,
-        "phi" : gen_child_pairs.LeadGenChild.phi,
-        "mass" : gen_child_pairs.LeadGenChild.mass
-    })
-
-    sublead_gen_child_p4 = vector.awk({
-        "pt" : gen_child_pairs.SubleadGenChild.pt,
-        "eta" : gen_child_pairs.SubleadGenChild.eta,
-        "phi" : gen_child_pairs.SubleadGenChild.phi,
-        "mass" : gen_child_pairs.SubleadGenChild.mass
-    })
-
-    gen_child_pairs[("GenParent", "dR")] = lead_gen_child_p4.deltaR(sublead_gen_child_p4) 
+    gen_child_pairs[("GenParent", "Child_1_Id")] = abs(gen_child_pairs.LeadGenChild.pdgId) 
+    gen_child_pairs[("GenParent", "Child_2_Id")] = abs(gen_child_pairs.SubleadGenChild.pdgId) 
 
     if awkward.any(awkward.num(gen_child_pairs) >= 2):
         gen_child_pairs = gen_child_pairs[awkward.argsort(gen_child_pairs.GenParent.pt, ascending = False, axis = 1)] 
 
-    return gen_child_pairs    
+    return gen_child_pairs   
