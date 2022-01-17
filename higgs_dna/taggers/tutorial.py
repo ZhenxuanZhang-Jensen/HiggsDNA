968e4dcb9fdbb95177f2339e5c945e879efa7e87
smay <smay@cern.ch>
2022-01-17 12:37:14 -0800

    Add example for more complex selections + cuts

--- higgs_dna/taggers/tutorial.py
+++ higgs_dna/taggers/tutorial.py
@@ -116,6 +116,35 @@ class TTHPreselTagger(Tagger):
 
         bjets = jets[awkward.argsort(jets.btagDeepFlavB, axis = 1, ascending = False)]
 
+        # Z-veto
+        # Register as `vector.Momentum4D` objects so we can do four-vector operations with them
+        electrons = awkward.Array(electrons, with_name = "Momentum4D")
+        muons = awkward.Array(muons, with_name = "Momentum4D")
+
+        # Construct di-electron/di-muon pairs
+        ee_pairs = awkward.combinations(
+                electrons, # objects to make combinations out of
+                2, # how many objects go in a combination
+                fields = ["LeadLepton", "SubleadLepton"] # can access these as e.g. ee_pairs.LeadLepton.pt
+        )
+        mm_pairs = awkward.combinations(muons, 2, fields = ["LeadLepton", "SubleadLepton"])
+    
+        # Concatenate these together
+        z_cands = awkward.concatenate([ee_pairs, mm_pairs], axis = 1)
+        z_cands["ZCand"] = z_cands.LeadLepton + z_cands.SubleadLepton # these add as 4-vectors since we registered them as "Momentum4D" objects
+
+        # Make Z candidate-level cuts
+        os_cut = z_cands.LeadLepton.charge * z_cands.SubleadLepton.charge == -1
+        mass_cut = (z_cands.ZCand.mass > 86.) & (z_cands.ZCand.mass < 96.)
+        z_cut = os_cut & mass_cut
+        z_cands = z_cands[z_cut] # OSSF lepton pairs with m_ll [86., 96.]
+
+        # Make event level cut
+        has_z_cand = awkward.num(z_cands) >= 1 # veto any event that has a Z candidate
+        ee_event = awkward.num(electrons) >= 2
+        mm_event = awkward.num(muons) >= 2
+        z_veto = ~(has_z_cand & (ee_event | mm_event))
+
         # Preselection
         n_electrons = awkward.num(electrons)
         n_muons = awkward.num(muons)
@@ -129,7 +158,11 @@ class TTHPreselTagger(Tagger):
         # Leptonic presel
         leptonic = (n_leptons >= 1) & (n_jets >= 2)
 
-        presel_cut = hadronic | leptonic
+        presel_cut = (hadronic | leptonic) & (z_veto)
+        self.register_cuts(
+            names = ["hadronic presel", "leptonic presel", "z veto", "all"],
+            results = [hadronic, leptonic, z_veto, presel_cut]
+        )
 
         return presel_cut, syst_events
     
