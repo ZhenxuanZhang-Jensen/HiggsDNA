11fd4897acc25019db12cbf83d0f72d641ac7819
Zhenxuan <zhenxuan.zhang@cern.ch>
2022-03-01 08:55:31 +0100

    clone from official HiggsDNA

---
+++ output/Data_2016/job_3/Data_2016_executable_job3.py
@@ -0,0 +1,13 @@
+import json
+import os
+from higgs_dna.utils.logger_utils import setup_logger
+from higgs_dna.analysis import run_analysis
+
+logger = setup_logger('DEBUG')
+config_file = '/afs/cern.ch/user/z/zhenxuan/HiggsDNA/output/Data_2016/job_3/Data_2016_config_job3.json'
+if not os.path.exists(config_file):
+    config_file = os.path.split(config_file)[-1]
+with open(config_file, 'r') as f_in:
+    config = json.load(f_in)
+
+run_analysis(config)
