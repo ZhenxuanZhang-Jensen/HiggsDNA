f449d4e9a3317b61b222916382729c5e2364fd06
shsong <shaowei.song@cern.ch>
2023-08-12 13:14:33 +0200

    run sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash   
+python scripts/run_analysis.py --log-level "DEBUG" --config "metadata/tutorial/HHWW_preselection_FHSL.json" --output_dir "/eos/user/s/shsong/hhwwggFHSL/" --batch_system "condor" 
